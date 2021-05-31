90f1be0f1275e0cba09e1b2d83791c681ddce1ab
Ronik22 <ronikbhattacharjee22112000@gmail.com>
2021-06-01 01:41:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,28 +10,31 @@ let circle = {
 	dy: 5,
 };
 
-
 let player = {
 	x: 200,
 	y: canvas.height - 20,
 	width: 100,
-	height: 10,
+	height: 15,
 	speed: 10,
 	dx: 0,
 	dy: 0,
 };
 
+let blocks = {
+	x: 13,
+	y: 10,
+	width: 53,
+	height: 15,
+	gap: 5,
+	countX: 10,
+	countY: 3
+};
+
 function updateScore() {
-	score = (parseInt(score) + 10).toString();
+	score = (parseInt(score) + 1).toString();
 	document.getElementById("scoreNum").innerHTML = score;
 }
 
-function checkScore() {
-    if(score === "100"){
-        alert("Congratulations!!! You Won.");
-        reset();
-    }
-}
 
 function drawPlayer() {
 	ctx.beginPath();
@@ -40,11 +43,59 @@ function drawPlayer() {
 	ctx.fill();
 }
 
+var allBlocks = []
+
+function blockSetup() {
+	distX = 0;
+	distY = 0;
+	for(j=1;j<=blocks.countY;j=j+1) {
+		for(i=1;i<=blocks.countX;i=i+1) {
+			allBlocks.push({
+				x: blocks.x + distX,
+				y: blocks.y + distY,
+				status: 1
+			});
+			distX += blocks.width + blocks.gap;
+		}
+		distX = 0;
+		distY += blocks.height + blocks.gap;
+	}	
+	// console.log(allBlocks)
+}
+blockSetup();
+
+
+function drawBlocks() {
+	allBlocks.forEach((block) => {
+		if (!block.status) return;
+		ctx.beginPath();
+		ctx.rect(block.x, block.y, blocks.width, blocks.height);
+		ctx.fillStyle = "#0bd600";
+		ctx.fill();
+		ctx.closePath();
+	});
+}
+
+function collisionDetection() {
+	allBlocks.forEach((b) => {
+		if (!b.status) return;
+
+		var inBricksColumn = circle.x > b.x && circle.x < b.x + blocks.width;
+		var inBricksRow = circle.y > b.y && circle.y < b.y + blocks.height;
+		if (inBricksColumn && inBricksRow) {
+			circle.dy *= -1;
+			b.status = 0;
+			updateScore();
+		}
+	});
+}
+
 function drawCircle() {
 	ctx.beginPath();
 	ctx.arc(circle.x, circle.y, circle.size, 0, Math.PI * 2);
 	ctx.fillStyle = "rgb(113, 24, 255)";
 	ctx.fill();
+	ctx.closePath();
 }
 
 function newPos() {
@@ -68,16 +119,7 @@ function clear() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-function update() {
-	clear();
-	drawCircle();
-	drawPlayer();
-	newPos();
-
-	// change position
-	circle.x += circle.dx;
-	circle.y += circle.dy;
-
+function wallDetection() {
 	// Detect side walls
 	if (circle.x + circle.size > canvas.width || circle.x - circle.size < 0) {
 		circle.dx *= -1;
@@ -87,24 +129,45 @@ function update() {
 	if (circle.y + circle.size > canvas.height || circle.y - circle.size < 0) {
 		circle.dy *= -1;
 	}
+}
+
+
+function update() {
+	clear();
+	drawBlocks();
+	drawCircle();
+	drawPlayer();
+	newPos();
+	collisionDetection();
+
+	// change position
+	circle.x += circle.dx;
+	circle.y += circle.dy;
+
+	wallDetection();
 
 	// score reset condition
 	if (circle.y + circle.size > canvas.height) {
-		score = "0";
-		document.getElementById("scoreNum").innerHTML = score;
+		clickedOnce = true;
+		return;
+		// score = "0";
+		// document.getElementById("scoreNum").innerHTML = score;
 	}
 
 	// Detect collision between player and circle
 	if (
-		circle.y + circle.size === player.y + player.height &&
-		circle.x > player.x &&
-		circle.x < player.x + player.width
-	) {
-		updateScore();
+		circle.y + circle.size === player.y &&
+		circle.x + circle.size > player.x &&
+		circle.x - circle.size < player.x + player.width
+	) {	
 		circle.dy *= -1;
 	}
 
-    checkScore();
+    if(score === (blocks.countX * blocks.countY).toString()){
+        alert("Congratulations!!! You Won.");
+		clickedOnce = true;
+        return;
+    }
 
 	requestAnimationFrame(update);
 }
@@ -151,25 +214,7 @@ function keyUp(e) {
 }
 
 function reset(){
-    score = "0";
-	document.getElementById("scoreNum").innerHTML = score;
-    circle = {
-        x: 100,
-        y: 100,
-        size: 15,
-        dx: 5,
-        dy: 5,
-    };
-    
-    player = {
-        x: 200,
-        y: canvas.height - 20,
-        width: 100,
-        height: 10,
-        speed: 10,
-        dx: 0,
-        dy: 0,
-    };
+	document.location.reload();
 }
 
 document.addEventListener("keydown", keyDown);
@@ -182,3 +227,5 @@ document.getElementById("start").onclick = () => {
 		update();
 	}
 };
+
+// update();
